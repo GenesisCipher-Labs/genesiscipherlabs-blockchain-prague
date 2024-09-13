72b6e84e821abd581ff2c12fda8d790888560569
ghapaaghupp <vidhanmangla@gmail.com>
2024-09-14 01:55:54 +0530

    chore: fix decrypt_private_key

--- decrypt_private_key.js
+++ decrypt_private_key.js
@@ -2,28 +2,30 @@
 const keythereum = require('keythereum');
 
 const keystore = {
-  "address": "7d441d18b79898449be8b05d1077308bc563669c",
+  "address": "Ethereum account address associated with this keystore",
   "crypto": {
-    "cipher": "aes-128-ctr",
-    "ciphertext": "caaafe9edc40fcd39b7c4cd0b5652b0f4d74a9e633866c6891f7ddbe70462d8d",
+    "cipher": "The encryption algorithm used to secure the private key",
+    "ciphertext": "The encrypted private key data",
     "cipherparams": {
-      "iv": "fb81dd87df6a843755095dd8bc9aacb3"
+      "iv": "initialisation_vector" // Initialisation Vector (IV) used for the cipher
     },
-    "kdf": "scrypt",
+    "kdf": "scrypt", // The Key Derivation Function (KDF) used to derive the encryption key from the password
     "kdfparams": {
-      "dklen": 32,
-      "n": 262144,
-      "p": 1,
-      "r": 8,
-      "salt": "6366027755683cd5abb63d99f5f438151001a0c7ad0a612a2d9e9853cea3c936"
+      "dklen": 32, // Key length derived from the KDF
+      "n": 262144, // KDF cost parameter
+      "p": 1, // KDF parallelisation parameter
+      "r": 8, // KDF block size parameter
+      "salt": "random_salt_value" // Random salt used for the KDF
     },
-    "mac": "fab50ee501c7e3f7120529e3d9aa4ed6f183a0ed13cdcc2af6e8767b3c089f7d"
+    "mac": "message_authentication_code" // Message Authentication Code (MAC) to verify the integrity of the data
   },
-  "id": "4d47ce44-6551-4b9d-b30a-49a956de6638",
-  "version": 3
+  "id": "unique_keystore_id", // Unique identifier for the keystore
+  "version": 3 // Keystore version
 };
 
-const password = "1"; // replace with the password for the keystore
+// Password used to unlock the keystore and decrypt the private key
+const password = "your_keystore_password"; // replace with the password for the keystore
 
+// This recovers the private key from the keystore using the password
 const privateKey = keythereum.recover(password, keystore);
-console.log("Private Key:", privateKey.toString("hex"));
+console.log("Private Key:", privateKey.toString("hex")); // Displays the decrypted private key
